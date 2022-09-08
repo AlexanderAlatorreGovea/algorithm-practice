5552686bdfda003e65fc773e4d449360d3b8034d
Alexander <Alexander_alatorre1993@hotmail.com>
2022-09-08 17:57:40 -0600

    added bst solution

--- find-closest-bst.js
+++ find-closest-bst.js
@@ -2,20 +2,40 @@ function findClosestBST(tree, target) {
   return findClosestBSTHelper(tree, target, tree.value);
 }
 
-function findClosestBSTHelper(tree, target, closest) {
-  let currentNode = tree;
+// function findClosestBSTHelper(tree, target, closestValue) {
+//   let currentNode = tree;
 
-  while (currentNode) {
-    if (
-      Math.abs(target - closestValue) > Math.abs(target - currentNode.value)
-    ) {
-      closestValue = currentNode.value;
+//   while (currentNode) {
+//     if (
+//       Math.abs(target - closestValue) > Math.abs(target - currentNode.value)
+//     ) {
+//       closestValue = currentNode.value;
+//     }
+
+//     if (target < currentNode.value) {
+//       currentNode = currentNode.left;
+//     } else if (target > currentNode.value) {
+//       currentNode = currentNode.right;
+//     } else {
+//       break;
+//     }
+//   }
+
+//   return closestValue;
+// }
+
+function closestValueHelper(tree, target, closestValue) {
+  let node = tree;
+
+  while (node) {
+    if (Math.abs(target - closestValue) > Math.abs(target - node.value)) {
+      closestValue = node.value;
     }
 
-    if (target < currentNode.value) {
-      currentNode = currentNode.left;
-    } else if (target > currentNode.value) {
-      currentNode = currentNode.right;
+    if (target > node.value) {
+      node = node.right;
+    } else if (target < node.value) {
+      node = node.left;
     } else {
       break;
     }
