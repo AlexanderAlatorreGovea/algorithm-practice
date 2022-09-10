662a141aa98a037aeb4cef3ce40ae1a3b173fd60
Alexander <Alexander_alatorre1993@hotmail.com>
2022-09-09 19:36:39 -0600

    added class photo

---
+++ classPhoto.js
@@ -0,0 +1,22 @@
+function classPhotos(redShirtHeights, blueShirtHeights) {
+  // Write your code here.
+  redShirtHeights.sort((a, b) => a - b);
+  blueShirtHeights.sort((a, b) => a - b);
+
+  const tallestStudents =
+    redShirtHeights[0] > blueShirtHeights[0] ? "RED" : "BLUE";
+
+  for (let i = 0; i < blueShirtHeights.length; i++) {
+    if (tallestStudents === "RED") {
+      if (redShirtHeights[i] <= blueShirtHeights[i]) {
+        return false;
+      }
+    } else if (tallestStudents === "BLUE") {
+      if (redShirtHeights[i] >= blueShirtHeights[i]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
